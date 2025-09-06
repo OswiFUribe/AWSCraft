a99fd8c93558fce7ac13bd88dbf7741427322eb6
Oswaldo Fonseca <oe.fonsecauribe@ugto.,mx>
2025-09-06 09:05:54 -0600

    fix install_minecraft.sh

--- setup/install_minecraft.sh
+++ setup/install_minecraft.sh
@@ -16,20 +16,24 @@ echo "[INFO] Configurando JAVA_HOME y PATH..."
 echo "export JAVA_HOME=/opt/jdk-21+35" >> ~/.bash_profile
 echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bash_profile
 
-# 4. Crear la carpeta para Minecraft y entrar en ella
+# 4. Limpieza
+echo "[INFO] Limpiando archivos temporales..."
+rm -f OpenJDK21U-jdk_x64_linux_hotspot_21_35.tar.gz
+
+# 5. Crear la carpeta para Minecraft y entrar en ella
 echo "[INFO] Creando carpeta ~/minecraft-server"
 mkdir -p minecraft-server/
 cd minecraft-server/
 
-# 5. Descargar Fabric server installer (versión 1.21.1 como ejemplo)
+# 6. Descargar Fabric server installer (versión 1.21.1 como ejemplo)
 echo "[INFO] Descargando Fabric..."
 curl --create-dirs -o "fabric-server-launch.jar" -sSLO https://meta.fabricmc.net/v2/versions/loader/1.21.8/0.17.2/1.1.0/server/jar
 
-# 6. Aceptar EULA automáticamente
+# 7. Aceptar EULA automáticamente
 echo "[INFO] Aceptando EULA..."
 echo "eula=true" > eula.txt
 
-# 7. Crear un script para iniciar el servidor
+# 8. Crear un script para iniciar el servidor
 echo "[INFO] Creando script para ejecutar el servidor..."
 cat > run.sh <<'EOF'
 #!/usr/bin/env bash
@@ -37,9 +41,7 @@ java -Xmx2G -Xms1G -jar fabric-server-launch.jar nogui
 EOF
 chmod +x run.sh
 
-# 8. Limpieza
-echo "[INFO] Limpiando archivos temporales..."
-rm -f OpenJDK21U-jdk_x64_linux_hotspot_21_35.tar.gz
+
 
 echo "[INFO] Instalación completa."
 echo "Usa './run.sh' dentro de minecraft-server/ para iniciar el servidor."
